735b4af3c9cbeff18f27ceb9e9eedbca87d5c6e1
DimitrisGeorgiou00 <45600170+DimitrisGeorgiou00@users.noreply.github.com>
2020-11-13 23:39:04 +0200

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -10,3 +10,4 @@ interface IERC20 {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
+//wtf
